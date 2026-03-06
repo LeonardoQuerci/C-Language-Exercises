0f8ebc9ace200e1a2a56554ee3a26d82516a0a7e
Leonardo Querci <leonardoquerciprado@gmail.com>
2026-03-06 09:01:32 -0300

    Add exercises from The self pointer

---
+++ The 'Self' Pointer/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "timer.h"
+
+int main() {
+    int initial_seconds, amount_to_add;
+    scanf("%d", &initial_seconds);
+    scanf("%d", &amount_to_add);
+    
+    // TODO: Create a Timer and initialize it with initial_seconds
+    Timer h;
+    h.seconds = initial_seconds;
+    // TODO: Call timer_tick to add amount_to_add seconds
+    timer_tick(&h,amount_to_add);
+    // TODO: Print the current seconds in format: "After tick: {seconds}"
+    printf("After tick: %d\n",h.seconds);
+    // TODO: Call timer_reset to reset the timer
+    timer_reset(&h);
+    // TODO: Print the seconds again in format: "After reset: {seconds}"
+    printf("After reset: %d",h.seconds);
+    return 0;
+}
+
