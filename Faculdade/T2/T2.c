fd519c9758268ab48fc8ad1fe4f6ed3ead6f01a5
Leonardo Querci <leonardoquerciprado@gmail.com>
2026-06-06 14:31:43 -0300

    Reaizando mais exercicios

--- Faculdade/T2/T2.c
+++ Faculdade/T2/T2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct Pessoa{
 	char cpf[20];
@@ -22,15 +23,15 @@ Arvore treeBi;
 void pedirDados(){
 	Pessoa p;
 	printf("Digite seu CPF: ");
-	scanf(" %[^\n]s", p.cpf);
+	scanf(" %[^\n]", p.cpf);
 	printf("Digite seu nome: ");
-	scanf(" %[^\n]s", p.nome);
+	scanf(" %[^\n]", p.nome);
 	printf("Digite seu email: ");
-	scanf(" %[^\n]s", p.email);
+	scanf(" %[^\n]", p.email);
 	printf("Digite seu celular: ");
-	scanf(" %[^\n]s", p.celular);
+	scanf(" %[^\n]", p.celular);
 	printf("Digite seu profissão: ");
-	scanf(" %[^\n]s", p.profissao);
+	scanf(" %[^\n]", p.profissao);
 	
 	FILE * arquiBin = fopen("./arquivoBinario.dat", "a+b");
 	fwrite(&p,sizeof(Pessoa),1,arquiBin);
@@ -39,7 +40,7 @@ void pedirDados(){
 
 void inserir(No **a, No n){
 	if (*a==NULL){
-		*a = (No *)malloc(sizeof(No));
+		*a = (No*)malloc(sizeof(No));
 		n.esquerda=NULL;
 		n.direita=NULL;
 		**a = n;
@@ -58,24 +59,67 @@ void carregarDados(){
 	FILE * arquiBin = fopen("./arquivoBinario.dat", "rb");
 	rewind(arquiBin);
 	//loop
-	while(verificar)){
+	while(verificar){
 		indi = ftell(arquiBin);
 		Pessoa p;
 		verificar = fread(&p,sizeof(Pessoa),1,arquiBin);
-		No n1={p.cpf, indi};
+		if (verificar ==0)break;
+		No n1;
+		strcpy(n1.cpf,p.cpf);
+		n1.indice = indi;
 		inserir(&treeBi, n1);
 	}
 	fclose(arquiBin);
 }
 
+int comparar(No **a, char cpfDesejado[20]){
+	if ((*a) == NULL)return -1;
+	int comp = strcmp(cpfDesejado, (*a)->cpf);
+	if (comp<0)return comparar(&(*a)->esquerda,cpfDesejado);
+	else if (comp>0)return comparar(&(*a)->direita,cpfDesejado);
+	else return (*a)->indice;
+}
+
 void localizar(){
+	char cpfDesejado[20];
+	printf("Qual o cpf que deseja consultar ---> ");
+	scanf("%s",cpfDesejado); //se algo der errado
+	int indiceEncontrado = comparar(&(treeBi),cpfDesejado);
+	
+	if (indiceEncontrado != -1){
+		FILE * arquiBin = fopen("./arquivoBinario.dat", "rb");
+		rewind(arquiBin);
+		fseek(arquiBin,indiceEncontrado,SEEK_SET);
+		Pessoa aux;
+		fread(&aux,sizeof(Pessoa),1,arquiBin);
+		printf("%s | %s | %s | %s | %s\n",aux.cpf,aux.nome,aux.email,aux.celular, aux.profissao);
+	}else{
+		printf("******* CPF NAO ENCONTRADO *******");
+	}
 }
 
-void gerarRelatorio(){
+void ordem(Arvore a, FILE * arq, FILE * arquiRel){ 
+	if(a!= NULL){
+			ordem(a->esquerda,arq,arquiRel);
+			
+			fseek(arq,a->indice,SEEK_SET);
+			Pessoa aux;
+			fread(&aux,sizeof(Pessoa),1,arq);
+			fprintf(arquiRel,"%s | %s | %s\n",aux.cpf,aux.nome,aux.profissao);
+			ordem(a->direita,arq,arquiRel);
+	}
 }
 
+void gerarRelatorio(Arvore a){
+	FILE * arquiBin = fopen("./arquivoBinario.dat", "rb");
+	rewind(arquiBin);
+	FILE * arquiRel = fopen("./arquivoRelatorio.txt", "w+");
+	ordem(a,arquiBin, arquiRel);
+	
+}
 int main(){
-	pedirDados();
+	carregarDados();
+	gerarRelatorio(treeBi);
 
 	return 0;
 }
